2ceea31cfedac67a790cc2750bb545fd7fa1b4a6
pokotyamu <pokotyamu@gmail.com>
2016-06-17 20:25:01 +0900

    function 機能を切り出した

--- .zsh/all.zsh
+++ .zsh/all.zsh
@@ -1,3 +1,4 @@
 [ -f $HOME/.zsh/env.zsh ]  && source $HOME/.zsh/env.zsh
 [ -f $HOME/.zsh/alias.zsh ]  && source $HOME/.zsh/alias.zsh
 [ -f $HOME/.zsh/completion.zsh ]  && source $HOME/.zsh/completion.zsh
+[ -f $HOME/.zsh/function.zsh ]  && source $HOME/.zsh/function.zsh
