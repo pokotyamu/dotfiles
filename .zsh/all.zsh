5b38c385c5c7466bd1dc702acdbdd0158b7b1170
pokotyamu <pokotyamu@gmail.com>
2016-06-17 20:28:23 +0900

    色に関する設定を切り出した

--- .zsh/all.zsh
+++ .zsh/all.zsh
@@ -2,3 +2,4 @@
 [ -f $HOME/.zsh/alias.zsh ]  && source $HOME/.zsh/alias.zsh
 [ -f $HOME/.zsh/completion.zsh ]  && source $HOME/.zsh/completion.zsh
 [ -f $HOME/.zsh/function.zsh ]  && source $HOME/.zsh/function.zsh
+[ -f $HOME/.zsh/color.zsh ]  && source $HOME/.zsh/color.zsh
