de9dcd3f726035478f15b09ad69fb77e3ee6ca5d
pokotyamu <pokotyamu@gmail.com>
2016-06-17 20:16:33 +0900

    補完に関する機能を切り出した

--- .zsh/all.zsh
+++ .zsh/all.zsh
@@ -1,2 +1,3 @@
 [ -f $HOME/.zsh/env.zsh ]  && source $HOME/.zsh/env.zsh
 [ -f $HOME/.zsh/alias.zsh ]  && source $HOME/.zsh/alias.zsh
+[ -f $HOME/.zsh/completion.zsh ]  && source $HOME/.zsh/completion.zsh
