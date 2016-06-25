314a5062eba272777f1618de840b5f3b101f7717
pokotyamu <pokotyamu@gmail.com>
2016-06-25 14:41:47 +0900

    helm の導入

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,16 @@
+;; Macの場合
+(require 'cask)
+
+(cask-initialize)
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+
+;; Helm
+(require 'helm-config)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(helm-mode 1)
+
+;;ミニバッファでC-hをバックスペースに割り当て
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
