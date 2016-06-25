a98bf32a65b7333b43aea033f5693a5fc555f0c4
pokotyamu <pokotyamu@gmail.com>
2016-06-25 15:10:32 +0900

    helm 用のショートカット作成

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,12 @@
 ;; Helm
 (require 'helm-config)
 (global-set-key (kbd "C-c h") 'helm-mini)
+(define-key global-map (kbd "M-x")     'helm-M-x)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x C-r") 'helm-recentf)
+(define-key global-map (kbd "M-y")     'helm-show-kill-ring)
+(define-key global-map (kbd "C-c i")   'helm-imenu)
+(define-key global-map (kbd "C-x b")   'helm-buffers-list)
 (helm-mode 1)
 
 ;;ミニバッファでC-hをバックスペースに割り当て
