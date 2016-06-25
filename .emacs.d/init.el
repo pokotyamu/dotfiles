7dd356374f6b57f61f0342fc277087d6f244d756
pokotyamu <pokotyamu@gmail.com>
2016-06-25 15:33:11 +0900

    前から使っている設定群を持ってきた

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,3 +20,63 @@
 
 ;;ミニバッファでC-hをバックスペースに割り当て
 (define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+
+
+;;rubyのマジックコメント対策
+(setq ruby-insert-encoding-magic-comment nil)
+
+;;行数
+(global-linum-mode t)
+
+(require 'whitespace)
+(setq whitespace-style '(face           ; faceで可視化
+			 trailing       ; 行末
+			 tabs           ; タブ
+			 spaces         ; スペース
+			 empty          ; 先頭/末尾の空行
+			 space-mark     ; 表示のマッピング
+			 tab-mark
+			 ))
+
+(setq whitespace-display-mappings
+      '((space-mark ?\u3000 [?\u25a1])
+	;; WARNING: the mapping below has a problem.
+	;; When a TAB occupies exactly one column, it will display the
+	;; character ?\xBB at that column followed by a TAB which goes to
+	;; the next TAB column.
+	;; If this is a problem for you, please, comment the line below.
+	(tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+;; スペースは全角のみを可視化
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+
+;; 保存前に自動でクリーンアップ
+(setq whitespace-action '(auto-cleanup))
+
+(global-whitespace-mode 1)
+
+(defvar my/bg-color "#232323")
+(set-face-attribute 'whitespace-trailing nil
+		    :background my/bg-color
+		    :foreground "DeepPink"
+		    :underline t)
+(set-face-attribute 'whitespace-tab nil
+		    :background my/bg-color
+		    :foreground "LightSkyBlue"
+		    :underline t)
+(set-face-attribute 'whitespace-space nil
+		    :background my/bg-color
+		    :foreground "GreenYellow"
+		    :weight 'bold)
+(set-face-attribute 'whitespace-empty nil
+		    :background my/bg-color)
+
+;; Window 分割・移動を C-t で
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (if (>= (window-body-width) 270)
+	(split-window-horizontally-n 3)
+      (split-window-horizontally)))
+  (other-window 1))
+(global-set-key (kbd "C-t") 'other-window-or-split)
